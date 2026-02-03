ac34bab2aa6533137ba688dd740bfe5167f27dfd
Lexeigh Kolakowski <lexeighk@gmail.com>
2026-02-03 13:41:02 -0800

    added cat head and ears

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -3,12 +3,19 @@ registerSketch('sk2', function (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
+
   p.draw = function () {
     p.background(220);
     p.fill(100, 150, 240);
     p.textSize(32);
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    p.fill('black');
+    p.ellipse(150, 150, 100, 100);
+    p.triangle(100, 150, 200, 150, 100, 50)
+    p.triangle(200, 150, 100, 150, 200, 50)
+
+
   };
+
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
