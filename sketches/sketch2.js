bb060caaefd4bb7a342b64a47d4dfd5bcdaa122e
Lexeigh Kolakowski <lexeighk@gmail.com>
2026-02-03 14:44:42 -0800

    added sitting cat when in cat time

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -4,7 +4,7 @@ registerSketch('sk2', function (p) {
     p.createCanvas(p.windowWidth, p.windowHeight);
 
     let button = p.createButton('Enter Cat Time');
-    button.position(300, 100);
+    button.position(300, 150);
 
     button.mousePressed(startCatTime);
   };
@@ -25,10 +25,21 @@ registerSketch('sk2', function (p) {
     p.textSize(32);
     p.textAlign(p.CENTER, p.CENTER);
     p.fill('black');
+    // Cat Head
     p.ellipse(150, 150, 100, 100);
-    p.triangle(100, 150, 200, 150, 100, 50)
-    p.triangle(200, 150, 100, 150, 200, 50)
+    p.triangle(100, 150, 180, 150, 100, 75)
+    p.triangle(200, 150, 120, 150, 200, 75)
 
+    // Cat body, changes on if in cat time
+    if (inCatTime == true) {
+      p.ellipse(150, 250, 125, 200);
+      p.push();
+      p.strokeWeight(15);
+      p.line(160, 340, 285, 285);
+      p.pop();
+    } 
+
+    // Cat Whiskers
     p.push();
     p.stroke('white');
     p.strokeWeight(4);
