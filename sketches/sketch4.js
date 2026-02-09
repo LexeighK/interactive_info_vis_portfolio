f36afa1d294186b28c921f42ef3083edde2d747d
Lexeigh Kolakowski <lexeighk@gmail.com>
2026-02-08 23:55:12 -0800

    added clock element

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -17,9 +17,36 @@ registerSketch('sk4', function (p) {
     p.noStroke();
     p.textSize(48);
     p.textAlign(p.LEFT, p.TOP);
-    p.text('Postcard Clock', 80, 60);
+    p.textSize(40);
+    p.text('Postcard Clock', 65, 70);
 
+    // Stamp
+    p.fill(255, 100, 100);
+    p.stroke(200, 50, 50);
+    p.strokeWeight(3);
+    p.rect(p.width - 150, 70, 80, 80, 10);
+    
+    // Address lines to show date and time
+    const startX = p.width - 400;
+    let startY = 350;
+    const lineSpacing = 30;
+    p.stroke(0);
+    p.strokeWeight(1);
 
+    for (let i = 0; i < 3; i++) {
+      p.line(startX, startY + i * lineSpacing, startX + 300, startY + i * lineSpacing);
+    }
+
+    const dateStr = p.day() + '/' + (p.month()) + '/' + p.year();
+    const timeStr = p.nf(p.hour(), 2) + ':' + p.nf(p.minute(), 2) + ':' + p.nf(p.second(), 2);
+
+    p.noStroke();
+    p.fill(0);
+    p.textSize(16);
+    p.textAlign(p.LEFT, p.CENTER);
+    p.text(dateStr, startX + 5, startY + lineSpacing / 2); 
+    p.text(timeStr, startX + 5, startY + 1.5 * lineSpacing); 
+  
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
