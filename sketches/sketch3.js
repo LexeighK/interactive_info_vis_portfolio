240ddfa391942c621593f89537b91c1178bb804f
Lexeigh Kolakowski <lexeighk@gmail.com>
2026-02-08 22:40:11 -0800

    add sketching feature

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -3,14 +3,9 @@ registerSketch('sk3', function (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
+  let scribbles = [];
   p.draw = function () {
     p.background(240, 200, 200);
-
-    // getting time
-    let h = p.hour();
-    let m = p.minute();
-
-    let timeText = p.nf(h, 2) + ":" + p.nf(m, 2);
 
     p.fill(100, 150, 240);
     p.stroke(100, 150, 240);
@@ -29,12 +24,33 @@ registerSketch('sk3', function (p) {
     p.stroke(0);
     p.rect(230, 230, 160, 100, 10);
 
-    // putting time on paper
-    p.fill(0);
-    p.noStroke();
-    p.textAlign(p.CENTER, p.CENTER);
-    p.textSize(24);
-    p.text(timeText, 310, 280);
+    // drawing on paper 
+    let startX = p.random(240, 350);
+    let startY = p.random(240, 320);
+    let length = p.random(10, 40);
+    let r = p.random(0, 180)
+    let g = p.random(0, 180)
+    let b = p.random(0, 180)
+    let linePoints = [];
+
+    for (let i = 0; i < length; i++) {
+      let x = startX + i;
+      let y = startY + p.noise(i * 0.1, p.frameCount * 0.05) * 10 - 5;
+      linePoints.push({x, y});
+    }
+
+    scribbles.push({ points: linePoints, color: [r, g, b] });
+
+    for (let l of scribbles) {
+      p.stroke(...l.color);
+      p.strokeWeight(2);
+      p.beginShape();
+      for (let pt of l.points) {
+        p.vertex(pt.x, pt.y);
+      }
+      p.endShape();
+    }
+    
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
