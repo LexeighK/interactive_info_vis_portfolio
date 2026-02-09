b92a9061aeefd3c0b4624ac6faeddd0a3c2a2e8c
Lexeigh Kolakowski <lexeighk@gmail.com>
2026-02-08 23:33:52 -0800

    added postcard background

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -5,10 +5,21 @@ registerSketch('sk4', function (p) {
   };
   p.draw = function () {
     p.background(200, 240, 200);
+
+    // Postcard background
+    p.fill(255, 250, 210); 
+    p.stroke(180, 120, 80);
+    p.strokeWeight(5);
+    p.rect(50, 50, p.width - 100, p.height - 100, 20);
+
+    // Title
     p.fill(30, 120, 40);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. C', p.width / 2, p.height / 2);
+    p.noStroke();
+    p.textSize(48);
+    p.textAlign(p.LEFT, p.TOP);
+    p.text('Postcard Clock', 80, 60);
+
+
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
