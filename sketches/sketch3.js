ffaad7c5e2b8f7fe3d0c5f51ea9f5aa0d1446fbd
Lexeigh Kolakowski <lexeighk@gmail.com>
2026-02-08 23:07:27 -0800

    added so that sketches appear for 5 minutes when button is clicked

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,12 +1,36 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
+  let button;
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    // Add in a button to start the sketches
+    button = p.createButton('Start Sketching');
+    button.size(200, 50);
+    button.style('font-size', '22px');
+    button.position(300, 150);
+    button.mousePressed(startSketching);
   };
+
+  // make it so that sketches last for 5 minutes
+  let sketching = false;
+  let sketchStartTime;
+  const sketchDuration = 5 * 60 * 1000; 
+
   let scribbles = [];
+
+  // start the sketching when the button is pressed
+  function startSketching() {
+    sketching = true;
+    sketchStartTime = p.millis();
+    button.hide();
+  }
+
   p.draw = function () {
     p.background(240, 200, 200);
-
+    if (sketching == false) {
+      p.text("Draw for 5 minutes", 175, 25)
+    }
+    
     p.fill(100, 150, 240);
     p.stroke(100, 150, 240);
     
@@ -25,32 +49,39 @@ registerSketch('sk3', function (p) {
     p.rect(230, 230, 160, 100, 10);
 
     // drawing on paper 
-    let startX = p.random(240, 350);
-    let startY = p.random(240, 320);
-    let length = p.random(10, 40);
-    let r = p.random(0, 180)
-    let g = p.random(0, 180)
-    let b = p.random(0, 180)
-    let linePoints = [];
+    if (sketching) {
+      if (p.millis() - sketchStartTime > sketchDuration) {
+        sketching = false;
+        button.show(); 
+      } else {
+        let startX = p.random(240, 350);
+        let startY = p.random(240, 320);
+        let length = p.random(10, 40);
+        let r = p.random(0, 180)
+        let g = p.random(0, 180)
+        let b = p.random(0, 180)
+        let linePoints = [];
 
-    for (let i = 0; i < length; i++) {
-      let x = startX + i;
-      let y = startY + p.noise(i * 0.1, p.frameCount * 0.05) * 10 - 5;
-      linePoints.push({x, y});
-    }
+        for (let i = 0; i < length; i++) {
+          let x = startX + i;
+          let y = startY + p.noise(i * 0.1, p.frameCount * 0.05) * 10 - 5;
+          linePoints.push({x, y});
+        }
 
-    scribbles.push({ points: linePoints, color: [r, g, b] });
-
-    for (let l of scribbles) {
-      p.stroke(...l.color);
-      p.strokeWeight(2);
-      p.beginShape();
-      for (let pt of l.points) {
-        p.vertex(pt.x, pt.y);
+        scribbles.push({ points: linePoints, color: [r, g, b] });
       }
-      p.endShape();
-    }
     
+      for (let l of scribbles) {
+        p.stroke(...l.color);
+        p.strokeWeight(2);
+        p.beginShape();
+        for (let pt of l.points) {
+          p.vertex(pt.x, pt.y);
+        }
+        p.endShape();
+      }
+      
+    }
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
